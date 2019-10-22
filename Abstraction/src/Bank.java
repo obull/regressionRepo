855273943e729fc4b63c9ff86c0ba740e32aad0d
Rihanna <Rihanna@DESKTOP-6QQF9JU.fios-router.home>
2019-10-22 15:40:24 -0400

    this is mfirst coomite

---
+++ Abstraction/src/Bank.java
@@ -0,0 +1,23 @@
+
+public abstract class Bank {
+	
+  public abstract void loan(); // abstract method --no method body
+  
+  public void credit(){
+	System.out.println("Bank--Credit");   
+  }
+	
+	public void debit() {
+		System.out.println("Bank--debit");
+		
+		//PARTIAL ABSTRACTION
+		//WHAT IS ABSTRACTION IS ? --A; HIDING THE IMPLEMENTION LOGIC .
+
+	
+	
+	
+	
+	
+
+}
+}
---
+++ Abstraction/src/CitiBank.java
@@ -0,0 +1,10 @@
+
+
+
+public class CitiBank extends Bank {
+	public void loan(){
+		
+     System.out.println("Citi--LOAN Method");	
+
+}
+}
